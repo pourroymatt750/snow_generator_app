bc068280be337ddd08ae48a449ffad280d7602ca
Mattthew Pourroy <69867050+pourroymatt750@users.noreply.github.com>
2025-11-19 01:32:13 -0600

    update wsgi.py to run migrations

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
+import django
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
+# Run migrations automatically at startup
+try:
+  call_command('migrate', interactive=False)
+except Exception as e:
+  # Optional: print error, but don’t crash the app
+  print(f"Migration error: {e}")
 
 application = get_wsgi_application()
